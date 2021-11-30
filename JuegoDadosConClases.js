a3e1d8061c299ebb57e2bd9cf1ca5eb6fb3cec7c
AlfredoHernandez45 <90002912+AlfredoHernandez45@users.noreply.github.com>
2021-11-29 20:36:46 -0500

    Ultima version

--- JuegoDadosConClases.js
+++ JuegoDadosConClases.js
@@ -51,23 +51,26 @@ class JuegoDados{
     
     constructor(numeroJuego, j1, j2){
         this.numeroJuego = numeroJuego;
-        this.jugador1 = j1;
-        this.jugador2 = j2;
+        this.jugador1 = new Jugador(j1.name);
+        this.jugador2 = new Jugador(j2.name);
     }
     
-    get tirarDados(){
-        this.jugador = Math.round((Math.random() * 5) + 1);
-        return this.jugador;
+    tirarDados(){
+        this.jugador1.caraDado1 = Math.round((Math.random() * 5) + 1);
+        this.jugador1.caraDado2 = Math.round((Math.random() * 5) + 1);
+        this.jugador2.caraDado1 = Math.round((Math.random() * 5) + 1);
+        this.jugador2.caraDado2 = Math.round((Math.random() * 5) + 1);
+        //console.log(this.jugador1.caraDado1);
         
     }
 
-    determinaGanador (jugador1, jugador2) {
-        if ( ((jugador1.caraDado1 + jugador1.caraDado2) == 7)
-            && ((jugador2.caraDado1 + jugador2.caraDado2) != 7) )
-            return jugador1.name;
-        else if ( ((jugador2.caraDado1 + jugador2.caraDado2) == 7)
-            && ((jugador1.caraDado1 + this.jugador2.caraDado1) != 7) )
-            return jugador2.name;
+    determinaGanador () {
+        if ( ((this.jugador1.caraDado1 + this.jugador1.caraDado2) == 7)
+            && ((this.jugador2.caraDado1 + this.jugador2.caraDado2) != 7) )
+            return this.jugador1.name;
+        else if ( ((this.jugador2.caraDado1 + this.jugador2.caraDado2) == 7)
+            && ((this.jugador1.caraDado1 + this.jugador1.caraDado2) != 7) )
+            return this.jugador2.name;
         else return null;
     }
 }//Fin de CLASE JUEGOS
@@ -88,7 +91,38 @@ class torneoDados{
     #juegosGanadosJugador1 = 0;
     #juegosGanadosJugador2 = 0;
 
+    #setjuegoGanadoJugador1(n){
+        this.#juegosGanadosJugador1 = n;
+    }
+    #setjuegosGanadosJugador2(n){
+        this.#juegosGanadosJugador2 = n;
+    }
+    set juegosGanadosJugador1(n){
+        this.#setjuegoGanadoJugador1(n);//Hacer privado y sus métodos getter y setter
+        
+    }
+    set juegosGanadosJugador2(n){
+        this.#setjuegosGanadosJugador2(n);//Hacer privado y sus métodos getter y setter
+        
+    }
+
+    //geters
+    #getjuegoGanadoJugador1(){
+        return this.#juegosGanadosJugador1;
+    }
+
+    #getjuegosGanadosJugador2(){
+        return this.#juegosGanadosJugador2;
+    }
+    get juegosGanadosJugador1(){
+        return this.#getjuegoGanadoJugador1();
+    }
+    get juegosGanadosJugador2(){
+        return this.#getjuegosGanadosJugador2();
+    }
+    //
     constructor (){
+        
         this.jugadas = new Array();
     }
 
@@ -99,8 +133,53 @@ class torneoDados{
     }
 
     jugar(cont){
-        cont++;
-        this.jugadas.push(new JuegoDados(cont, this.jugador1, this.jugador2));
-    
+        
+        this.jugadas.push(new JuegoDados(cont+1, this.jugador1, this.jugador2));
+
+        this.jugadas[cont].tirarDados();
+        
+        var ganador = this.jugadas[cont].determinaGanador()
+
+        if(ganador == this.jugador1.name){
+            this.juegosGanadosJugador1++;
+            console.log("El ganador del juego "+ (cont+1) +" es: " + this.jugador1.name);
+        }
+        else if(ganador == this.jugador2.name){
+            console.log("El ganador del juego "+ (cont+1) +" es: " + this.jugador2.name);
+            this.juegosGanadosJugador2++;
+        }
+        
     }
+
+    #resultado(){
+        if(this.juegosGanadosJugador1 == 3){
+            return this.jugador1.name;
+            
+        }
+        else if(this.juegosGanadosJugador2 == 3){
+            return this.jugador2.name;
+            
+        }
+    }
+    get resultado(){
+        return this.#resultado();
+    }
+
+    set resultado(n){
+        this.#resultado = n;
+    }
+}
+var torneo = new torneoDados();
+torneo.crear("Pedro Sánchez", "Antonio Ramírez");
+
+var cont = 0;
+var ganadorTorneo = " ";
+
+while(ganadorTorneo == " "){
+    torneo.jugar(cont);
+    if(torneo.resultado != undefined){
+        ganadorTorneo = torneo.resultado;
+        console.log("\nEl ganador del torneo es "+ ganadorTorneo);
+    }
+    cont++;
 }
